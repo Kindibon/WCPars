18fd5eaa66142cb7a9ad714dd5345546156ea447
Kindibon <93122219+Kindibon@users.noreply.github.com>
2021-10-25 15:20:37 +0300

    Добавьте файлы проекта.

---
+++ WCPars/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace WCPars
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 34);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(423, 577);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(415, 549);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dps PVE (Mythic Raid)";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(7, 32);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ShowSelectionMargin = true;
+            this.richTextBox2.Size = new System.Drawing.Size(402, 511);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.ZoomFactor = 1.2F;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.richTextBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(415, 549);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Dps PVP 2v2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(330, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(3, 29);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(402, 514);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox4);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(415, 549);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Healer(Mythic Raid)";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(6, 32);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(403, 511);
+            this.richTextBox4.TabIndex = 2;
+            this.richTextBox4.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(334, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(632, 42);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(75, 45);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(134, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DPS/HPS Ranking";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(572, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 30);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "WOW Token Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(602, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "EU:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(602, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "US:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(350, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Clear ALL";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 623);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "WoW META status";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WCPars/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Net;
+
+namespace WCPars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }      
+        public void WowheadGold()
+        {
+            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = website.Load("https://www.wowhead.com/");   
+            var datalist3 = document.DocumentNode.SelectNodes("//span[@class='moneygold']").ToList();
+            foreach (var content in datalist3)
+            {
+                richTextBox1.Text += content.InnerText +Environment.NewLine;
+            }            
+        }
+        public void GetValuesPVE()
+        {
+            string modifiedString = string.Empty;
+            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();     
+            HtmlAgilityPack.HtmlDocument document2 = website.Load("https://www.wowmeta.com/shadowlands/dps-rankings");
+            var datalist2 = document2.DocumentNode.SelectNodes("//*[@id='rankings-chart']/div[1]/div[1]").ToList();
+            foreach (var content in datalist2)
+            {
+                richTextBox2.Text = content.InnerText;
+            }
+            foreach (string str in richTextBox2.Text.Split("\n"))
+            {
+                modifiedString += "        " + str;
+                //"                     " +
+            }
+            richTextBox2.Text = modifiedString;
+        }
+        public void GetValuesPVP()
+        {
+            string modifiedString1 = string.Empty;
+            HtmlAgilityPack.HtmlWeb website1 = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document3 = website1.Load("https://www.wowmeta.com/shadowlands/pvp-dps-rankings#2v2");
+            var datalist3 = document3.DocumentNode.SelectNodes("/html/body/main/div/div/div[2]/div[2]/div[2]").ToList();
+            foreach (var content1 in datalist3)
+            {
+                richTextBox3.Text = content1.InnerText;
+            }
+            foreach (string str in richTextBox3.Text.Split("\n"))
+            {
+                modifiedString1 += "                               " + str;
+                //"                     " +
+            }
+            richTextBox3.Text = modifiedString1;
+        }
+        public void GetValuesPVEHeal()
+        {
+            string modifiedString2 = string.Empty;
+            HtmlAgilityPack.HtmlWeb website2 = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document4 = website2.Load("https://www.wowmeta.com/shadowlands/healer-rankings");          
+            var datalist4 = document4.DocumentNode.SelectNodes("/html/body/main/div/div[1]/div[3]/div[5]/div[1]/div[1]").ToList();
+            foreach (var content2 in datalist4)
+            {
+                richTextBox4.Text = content2.InnerText;
+            }
+            foreach (string str3 in richTextBox4.Text.Split("\n"))
+            {               
+                modifiedString2 += "             " + str3;
+                //"                     " +
+            }
+            richTextBox4.Text = modifiedString2;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetValuesPVE();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WowheadGold();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetValuesPVEHeal();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetValuesPVP();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        //    richTextBox1.Clear();
+        //    richTextBox2.Clear();
+        //    richTextBox3.Clear();
+        //    richTextBox4.Clear();
+        }
+    }
+}
+
+
